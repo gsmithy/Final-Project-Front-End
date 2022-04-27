c2e461703b4b9e0db49f65e915f61429763a593a
Conor Wiley <wileyconro98@gmail.com>
2022-04-27 15:51:53 -0700

    Actually have bootstrap now

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => (
   <div>
     <h2>Login</h2>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">
+          @
+        </span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Username"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      ></input>
+    </div>
+
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Recipient's username"
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+      ></input>
+      <div class="input-group-append">
+        <span class="input-group-text" id="basic-addon2">
+          @example.com
+        </span>
+      </div>
+    </div>
+
+    <label for="basic-url">Your vanity URL</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3">
+          https://example.com/users/
+        </span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        id="basic-url"
+        aria-describedby="basic-addon3"
+      ></input>
+    </div>
+
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Amount (to the nearest dollar)"
+      ></input>
+      <div class="input-group-append">
+        <span class="input-group-text">.00</span>
+      </div>
+    </div>
+
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">With textarea</span>
+      </div>
+      <textarea class="form-control" aria-label="With textarea"></textarea>
+    </div>
   </div>
 );
 
