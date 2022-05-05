0eaff10f7b6e00503cdcb15b665aa01c8258719f
Dustin Kelley <kelleydustin2018@gmail.com>
2022-05-05 17:31:58 -0500

    added the post request to the sign up route

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,110 +1,152 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const SignUp = () => {
+
+const SignUp = (history) => {
+  const [{
+    first_name, setFirstname,
+    last_name, setLastname,
+    user_name, setUsername,
+    password, setPassword,
+    email, setEmail
+  }] = useState(" ");
+
+  const createUser = (e) => {
+    e.preventDefault();
+
+    const req = {
+      first_name, 
+      last_name,
+      user_name,
+      password,
+      email,
+    }
+
+    axios.post('http://localhost3001:users', req)
+    .then(result => {
+      console.log(result.data);
+      history.push('login');
+    })
+
+  };
   return (
-    <div className="vh-100" >
-      <div className="container h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-lg-12 col-xl-11">
-            <div className="card text-black" >
-              <div className="card-body p-md-5">
-                <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                      Create Your Account!
-                    </p>
+    <form onSubmit={createUser}>
+    <h1>Sign Up</h1>
+    <label>Username</label>
+    <input type="text" className={user_name} onChange={e => setUsername(e.target.value)}/>
+    <label>firstname</label>
+    <input type="text" className={first_name} onChange={e => setFirstname(e.target.value)}/>
+    <label>firstname</label>
+    <input type="text" className={last_name} onChange={e => setLastname(e.target.value)}/>
+    <label>firstname</label>
+    <input type="text" className={password} onChange={e => setPassword(e.target.value)}/>
+    <label>firstname</label>
+    <input type="text" className={email} onChange={e => setEmail(e.target.value)}/>
+    <button>Create</button>
+    </form>
+    // <div className="vh-100" >
+    //   <div className="container h-100">
+    //     <div className="row d-flex justify-content-center align-items-center h-100">
+    //       <div className="col-lg-12 col-xl-11">
+    //         <div className="card text-black" >
+    //           <div className="card-body p-md-5">
+    //             <div className="row justify-content-center">
+    //               <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    //                 <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+    //                   Create Your Account!
+    //                 </p>
 
-                    <form className="mx-1 mx-md-4">
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example1c"
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="form3Example1c">
-                            What's Your Name?
-                          </label>
-                        </div>
-                      </div>
+    //                 <form className="mx-1 mx-md-4">
+    //                   <div className="d-flex flex-row align-items-center mb-4">
+    //                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+    //                     <div className="form-outline flex-fill mb-0">
+    //                       <input
+    //                         type="text"
+    //                         id="form3Example1c"
+    //                         className="form-control"
+    //                       />
+    //                       <label className="form-label" htmlFor="form3Example1c">
+    //                         What's Your Name?
+    //                       </label>
+    //                     </div>
+    //                   </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="form3Example3c">
-                            What's Your Email?
-                          </label>
-                        </div>
-                      </div>
+    //                   <div className="d-flex flex-row align-items-center mb-4">
+    //                     <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+    //                     <div className="form-outline flex-fill mb-0">
+    //                       <input
+    //                         type="email"
+    //                         id="form3Example3c"
+    //                         className="form-control"
+    //                       />
+    //                       <label className="form-label" htmlFor="form3Example3c">
+    //                         What's Your Email?
+    //                       </label>
+    //                     </div>
+    //                   </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="form3Example4c">
-                            Password
-                          </label>
-                        </div>
-                      </div>
+    //                   <div className="d-flex flex-row align-items-center mb-4">
+    //                     <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+    //                     <div className="form-outline flex-fill mb-0">
+    //                       <input
+    //                         type="password"
+    //                         id="form3Example4c"
+    //                         className="form-control"
+    //                       />
+    //                       <label className="form-label" htmlFor="form3Example4c">
+    //                         Password
+    //                       </label>
+    //                     </div>
+    //                   </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4cd"
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="form3Example4cd">
-                            Repeat your password
-                          </label>
-                        </div>
-                      </div>
+    //                   <div className="d-flex flex-row align-items-center mb-4">
+    //                     <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+    //                     <div className="form-outline flex-fill mb-0">
+    //                       <input
+    //                         type="password"
+    //                         id="form3Example4cd"
+    //                         className="form-control"
+    //                       />
+    //                       <label className="form-label" htmlFor="form3Example4cd">
+    //                         Repeat your password
+    //                       </label>
+    //                     </div>
+    //                   </div>
 
-                      <div className="form-check d-flex justify-content-center mb-5">
-                        <input
-                          className="form-check-input me-2"
-                          type="checkbox"
-                          value=""
-                          id="form2Example3c"
-                        />
-                        <label className="form-check-label" htmlFor="form2Example3">
-                          I agree all statements in{" "}
-                          <a href="#!">Terms of service</a>
-                        </label>
-                      </div>
+    //                   <div className="form-check d-flex justify-content-center mb-5">
+    //                     <input
+    //                       className="form-check-input me-2"
+    //                       type="checkbox"
+    //                       value=""
+    //                       id="form2Example3c"
+    //                     />
+    //                     <label className="form-check-label" htmlFor="form2Example3">
+    //                       I agree all statements in{" "}
+    //                       <a href="#!">Terms of service</a>
+    //                     </label>
+    //                   </div>
 
-                      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="button" className="btn btn-primary btn-lg">
-                          Register
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                      className="img-fluid"
-                      alt="Sample"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    //                   <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+    //                     <button type="button" className="btn btn-primary btn-lg">
+    //                       Register
+    //                     </button>
+    //                   </div>
+    //                 </form>
+    //               </div>
+    //               <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+    //                 <img
+    //                   src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+    //                   className="img-fluid"
+    //                   alt="Sample"
+    //                 />
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
