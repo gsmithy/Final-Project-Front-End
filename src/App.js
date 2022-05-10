e1cfa5799ccba429e5ac8e8cbd3ead05bc81915b
Dustin Kelley <kelleydustin2018@gmail.com>
2022-05-10 16:58:32 -0500

    cleaned up app

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/newpost" element={<NewPost />} />
-
           <Route exact path="/" element={<Home />} />
-
           <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<SignUp />} />
 
