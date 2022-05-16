79126c6f807260bb70a928ea8aae0d75de8365b4
Dustin Kelley <kelleydustin2018@gmail.com>
2022-05-16 12:25:04 -0500

    added get all posts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Profile from "./screens/Profile";
 import SignUp from "./screens/SignUp";
 import Header from "./components/Header";
 import Admin from "./screens/Admin";
+import AdminPosts from "./screens/AdminPosts";
 import Missing from "./screens/Missing";
+
 import PrivateRoutes from "./hooks/PrivateRoute";
 //import RequireAuth from "./components/RequireAuth";
 
@@ -28,9 +30,10 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<SignUp />} />
         {/* //Protected Routes */}
-          <Route element={<PrivateRoutes />}>
+          {/* <Route element={<PrivateRoutes />}> */}
             <Route path="/admin" element={<Admin />} />
-          </Route>
+            <Route path="/adminposts" element={<AdminPosts />} />
+          {/* </Route> */}
          {/* //Catch All */}
           <Route path="/missing" element={<Missing />} />
         </Routes>
--- src/screens/AdminPosts.js
+++ src/screens/AdminPosts.js
@@ -5,9 +5,9 @@ import { Nav } from "react-bootstrap";
 
 
 
-
-const Admin = () => {
-   const [users, setUsers] = useState([]);
+//Component is not rendering to page. "Undefined" error.
+const AdminPosts = () => {
+   const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         let token = localStorage.getItem("myJWT");
@@ -18,26 +18,26 @@ const Admin = () => {
             }
           }
 
-        axios.get('http://localhost:3001/admin', options)
-        .then(users => {
-            setUsers(users.data);
-            console.log(users);
+        axios.get('http://localhost:3001/admin/posts', options)
+        .then(posts => {
+            setPosts(posts.data);
+            console.log(posts);
         })
     }, []);
 
     return (
         <div>
-            <h1>All Users</h1>
+            <h1>All Posts</h1>
             <Nav className=" d-flex justify-content-beginning">
-              <Nav.Link href="/AdminPosts">Posts</Nav.Link>
+              <Nav.Link href="/Admin">Users</Nav.Link>
             </Nav>
             <ul>
-              {users.map(user => 
-                <Card key={user.user_name}>
-                  <Card.Header as="h5">User</Card.Header>
+              {posts.map(posts => 
+                <Card key={posts.id}>
+                  <Card.Header as="h5">{posts.user_name}</Card.Header>
                   <Card.Body>
-                  <Card.Title>{user.first_name}</Card.Title>
-                  <Card.Text>{user.last_name}</Card.Text>
+                  <Card.Title>{posts.description}</Card.Title>
+                  <Card.Text>{posts.location}</Card.Text>
                  
             <Button className="btn" variant="danger">
               delete
@@ -50,4 +50,4 @@ const Admin = () => {
     );
 };
 
-export default Admin;
+export default AdminPosts;
