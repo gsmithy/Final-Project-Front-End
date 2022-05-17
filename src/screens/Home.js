5f136eeb1254dfbf4e087fa7c87d84a572dac438
Greig Corbett-Smith <gsmith.and.company@gmail.com>
2022-05-17 12:45:30 +1200

    nothing to commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ApiCall from "../components/Bible";
 import PersonList from "../components/HomeDB";
 
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
         <PersonList />
+        <ApiCall />
     </div>
   );
 };
