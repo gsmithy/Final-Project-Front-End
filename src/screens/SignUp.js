6ec292bf4d1ce57d2689fe267a33e45bd3edcf4e
Conor Wiley <wileyconro98@gmail.com>
2022-05-22 16:46:55 -0700

    fixed footer at bottom of sign-up

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -38,7 +38,7 @@ const SignUp = () => {
   };
   return (
     
-    <div className="vh-100">
+    <div>
       <div className="container h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
